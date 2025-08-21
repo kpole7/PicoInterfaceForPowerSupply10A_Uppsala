b823656513473e572e115d220e4645d57fe3febd
kpole7 <kpole@slcj.uw.edu.pl>
2025-08-21 15:24:39 +0200

    Pulse wave generator (f=40kHz, ti=3us) completed;

--- source/power_source.c
+++ source/power_source.c
@@ -5,34 +5,28 @@
 #include "hardware/irq.h"
 #include "hardware/timer.h"
 
-#define OUT_PIN 	6
+#define OUT_PIN 		6
+#define DEBUG_PIN 		7
 
-#define PERIOD_US 	25
-#define TIME_ON		5
-#define TIME_OFF	(PERIOD_US-TIME_ON)
+#define PERIOD_US 		25
+#define PULSE_TIME_ON	3
 
-#define ALARM_NUM 	0
-#define ALARM_IRQ 	TIMER_IRQ_0
+#define ALARM_NUM 		0
+#define ALARM_IRQ 		TIMER_IRQ_0
 
 volatile uint64_t target;
-volatile bool pin_state = false;
 
 // Handler przerwania z hardware timer (alarm)
 void timer_irq() {
-    gpio_put(OUT_PIN, pin_state);
-    // Clear the alarm irq
-    hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
+	gpio_put(OUT_PIN, true);
 
-    if (pin_state){
-        target += TIME_ON;
-        timer_hw->alarm[ALARM_NUM] = (uint32_t) target;
-        pin_state = false;
-    }
-    else{
-        target += TIME_OFF;
-        timer_hw->alarm[ALARM_NUM] = (uint32_t) target;
-        pin_state = true;
-    }
+	// Clear the alarm irq
+	hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
+	target += PERIOD_US;
+	timer_hw->alarm[ALARM_NUM] = (uint32_t) target;
+
+	sleep_us( PULSE_TIME_ON );
+	gpio_put(OUT_PIN, false);
 }
 
 int main() {
@@ -40,8 +34,12 @@ int main() {
 	assert( ALARM_IRQ == timer_hardware_alarm_get_irq_num(timer_hw, ALARM_NUM));
 
     stdio_init_all();
+
     gpio_init(OUT_PIN);
     gpio_set_dir(OUT_PIN, GPIO_OUT);
+
+    gpio_init(DEBUG_PIN);
+    gpio_set_dir(DEBUG_PIN, GPIO_OUT);
 
     // Enable the interrupt for our alarm (the timer outputs 4 alarm irqs)
     hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM);
@@ -53,8 +51,8 @@ int main() {
     irq_set_enabled(ALARM_IRQ, true);
 
     while (true) {
-
-        tight_loop_contents();
+    	gpio_put(DEBUG_PIN, true);
+    	gpio_put(DEBUG_PIN, false);
     }
 }
 
