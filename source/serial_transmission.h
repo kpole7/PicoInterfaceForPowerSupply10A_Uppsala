1914537f9d579fbf74c02290336fbad3f0dc5b5e
kpole7 <kpole@slcj.uw.edu.pl>
2025-08-26 09:33:00 +0200

    Added comments (syntax of communication protocol with the master unit)

--- source/serial_transmission.h
+++ source/serial_transmission.h
@@ -4,9 +4,27 @@
 ///
 /// The module acts as a slave. It receives commands from the master unit and
 /// sends the responses.  The protocol consists from the following text commands:
-/// command			response
-/// * command: `MC\r\n`    response: `n.n\r\n`
-/// * command: `PCn\r\n`   response: `xyz`
+///
+/// 1.1. Command **Measure Current**: `MC\r\n`
+///
+/// 1.2. Exemplary response: `-10.34\r\n\n>`
+///
+/// 2.1. Exemplary command **Program Current**: `PC-5.67\r\n`
+///
+/// 2.2. Response: `\r\n\n>`
+///
+/// 3.1. Command **Place software revision**: `?M\r\n`
+///
+/// 3.2. Exemplary response: `Rev. abcdefghijklmnopqrst\r\n\n>`
+///
+/// 4.1. Command **Current DAC programming value**: `?C\r\n`
+///
+/// 4.2. Exemplary response: `-2.34\r\n\n>`
+///
+/// 5.1. Command **Logic output value**: `?Y\r\n`
+///
+/// 5.2. Exemplary response: `1\r\n\n>`
+///
 
 
 #ifndef SOURCE_SERIAL_TRANSMISSION_H_
