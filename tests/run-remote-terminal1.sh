3d435fe34a12e72b5400d08dc89a699304384d71
kpole7 <kpole@slcj.uw.edu.pl>
2025-10-10 14:31:47 +0200

    The script has been simplified;

--- tests/run-remote-terminal1.sh
+++ tests/run-remote-terminal1.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-SSH_CMD='ssh -X cyklotron@192.168.201.247'
-gnome-terminal -- bash -lc "$SSH_CMD; exec bash"
+gnome-terminal -- bash -lc "ssh -X cyklotron@192.168.201.247; exec bash"
 
