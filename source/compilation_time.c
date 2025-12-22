e151ab23c3295a47c14189fd951d297a35ad4740
kpole7 <kpole@slcj.uw.edu.pl>
2025-12-22 09:14:13 +0100

    recompiled in 'hardware' mode (not simulation mode); added a message during compilation indicating which mode is active;

--- source/compilation_time.c
+++ source/compilation_time.c
@@ -1,5 +1,14 @@
 /// @file compilation_time.c
 /// This file should be compiled every time the “make” command is invoked
 
+#include "config.h"
+
+#if SIMULATE_HARDWARE_PSU > 0
+#pragma message("Build: SIMULATE_HARDWARE_PSU=1 (simulation)")
+#else
+#pragma message("Build: SIMULATE_HARDWARE_PSU=0 (hardware)")
+#endif
+
+
 /// This text contains compilation date and time
 const char CompilationTime[30] = __DATE__ ", " __TIME__;
